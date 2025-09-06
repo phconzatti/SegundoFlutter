3fbd58d6f64b0adb675ea7ca190b816257f67c15
Pedro Henrique Schneider Conzatti <phconzatti@hotmail.com>
2025-09-06 14:43:36 -0300

    .

--- lib/views/pages/course_page.dart
+++ lib/views/pages/course_page.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/views/widgets/hero_widget.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
 
-class CoursePage extends StatelessWidget {
+class CoursePage extends StatefulWidget {
   const CoursePage({super.key});
+
+  @override
+  State<CoursePage> createState() => _CoursePageState();
+}
+
+class _CoursePageState extends State<CoursePage> {
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
+  void getData () async {
+    var url = Uri.https('bored-api.appbrewery.com', '/random');
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var jsonResponse = convert.jsonDecode(response.body) as Map<String, dynamic>;
+      var itemCount = jsonResponse['activity'];
+      print(itemCount);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
